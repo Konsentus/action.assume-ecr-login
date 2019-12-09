257c2b009b8287e785fb0814d36ae500a6144e91
Andy Medlicott <amedlicott11@gmail.com>
2019-12-09 13:07:26 +0000

    Added action output

--- entrypoint.sh
+++ entrypoint.sh
@@ -88,3 +88,10 @@ login_to_ecr || exit $?
 docker run -e COLLECTION_TYPE=public -e ENVIRONMENT=${INPUT_ENVIRONMENT} -e REPORT_DIR=/report \
   --rm -v `pwd`/report:/report ${INPUT_IMAGE}:${INPUT_ENVIRONMENT}
 
+docker_result = $?;
+if [ $docker_result -ne 0 ]; then
+  echo "Docker returned exit code $docker_result"
+  exit($docker_result);
+fi
+
+echo ::set-output name=report::`cat report/public-${INPUT_ENVIRONMENT}.html`
